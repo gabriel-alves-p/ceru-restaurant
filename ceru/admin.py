fa06e066c9e27b7e473b9d0ce8c2d55ab1584191
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-01-27 22:01:30 +0000

    install summernote and apply to requirements field for booking model in admin.py

--- ceru/admin.py
+++ ceru/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Guest, Booking
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+    """
+    A Class to select which admin fields I want
+    to apply Summernote to,
+    in this case: requirements in Guest model.
+    """
+    summernote_fields = ('requirements')
+
 
 admin.site.register(Guest)
-admin.site.register(Booking)
