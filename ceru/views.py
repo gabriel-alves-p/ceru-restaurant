d1eb1a726fb8ad350abc64b167e41a127d2f69a9
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-01-30 18:31:29 +0000

    create base.html and begin to add content

--- ceru/views.py
+++ ceru/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
-# Create your views here.
+
+class BookingDisplay(generic.ListView):
+    """
+    This class describes how to display
+    bookings for the User to see.
+    """
+    model = Booking
+    queryset = Booking.objects.order_by('-date')
+    template_name = 'dashboard.html'
