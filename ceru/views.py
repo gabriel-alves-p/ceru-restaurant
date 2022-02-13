15d760b9ee51dd5ddfe31a772b2d206d8a677988
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-02-13 17:46:09 +0000

    delete dahsboard and fix static files not being found in index.html bug

--- ceru/views.py
+++ ceru/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.base import TemplateView
 from .models import Booking
 
 
-class IndexView(generic.ListView):
+class HomeTemplateView(TemplateView):
     """
-    This class is the view for the
-    'home' page or 'index.html'.
+    Home page template view.
     """
-    model = Booking
-    queryset = Booking
     template_name = 'index.html'
-
-
-class BookingDisplay(generic.ListView):
-    """
-    This class describes how to display
-    bookings for the User to see.
-    """
-    model = Booking
-    queryset = Booking.objects.order_by('-date')
-    template_name = 'dashboard.html'
