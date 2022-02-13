97cb805b76640e7eccbb6518db0ff1a66cba466d
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-02-13 17:46:09 +0000

    delete dahsboard and fix static files not being found in index.html bug

--- ceru/urls.py
+++ ceru/urls.py
@@ -1,9 +1,7 @@
-from . import views
 from django.urls import path
+from .views import HomeTemplateView
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='home'),
-    path('accounts/login/dashboard/', views.BookingDisplay.as_view(), name='dashboard'),  # noqa
-    path('accounts/signup/dashboard/', views.BookingDisplay.as_view(), name='dashboard'),  # noqa
+    path('', HomeTemplateView.as_view(), name='home'),
 ]
--- ceru_restaurant/settings.py
+++ ceru_restaurant/settings.py
@@ -55,10 +55,10 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL = 'dashboard/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-SIGNUP_REDIRECT_URL = 'dashboard/'
+SIGNUP_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
