029990f67afe9763aa2be7bf8e50ab0f083f9d7f
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-02-17 19:48:55 +0000

    create all html templates and link with views.py and urls.py respectfully

--- ceru/views.py
+++ ceru/views.py
@@ -9,3 +9,32 @@ class HomeTemplateView(TemplateView):
     Home page template view.
     """
     template_name = 'index.html'
+
+
+class BookingView(TemplateView):
+    """
+    Booking form page template view.
+    """
+    template_name = 'booking_form.html'
+    model = Booking
+
+
+class EditBookingView(TemplateView):
+    """
+    Edit booking page template view.
+    """
+    template_name = 'edit_booking.html'
+
+
+class DashboardView(TemplateView):
+    """
+    User's dashboard page template view.
+    """
+    template_name = 'dashboard.html'
+
+
+class EditProfileView(TemplateView):
+    """
+    User's dashboard page template view.
+    """
+    template_name = 'edit_profile.html'
