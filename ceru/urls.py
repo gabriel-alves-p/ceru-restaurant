2da8dd3d41b1a555f39a79a65bbab6a397114100
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-02-17 19:48:55 +0000

    create all html templates and link with views.py and urls.py respectfully

--- ceru/urls.py
+++ ceru/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomeTemplateView
+from ceru import views
 
 
 urlpatterns = [
-    path('', HomeTemplateView.as_view(), name='home'),
+    path('', views.HomeTemplateView.as_view(), name='home'),
+    path('booking/', views.BookingView.as_view(), name='booking'),
+    path('edit-booking/', views.EditBookingView.as_view(), name='edit-booking'),  # noqa
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('edit-profile/', views.EditProfileView.as_view(), name='edit-profile'),  # noqa
 ]
