91b3d685a4ff4f235a16c34c8a2ce9ae84760802
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-02-18 09:19:39 +0000

    create signals.py and write function to create a profile when a user is created

--- ceru/apps.py
+++ ceru/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CeruConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ceru'
+
+    def ready(self):
+        import ceru.signals
