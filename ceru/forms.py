06241f8cf13cd63381549496e2ce2c4283622cdc
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-03-01 02:02:49 +0000

    add bootstrap classes to EditProfileForm in forms.py and update its view accordingly

--- ceru/forms.py
+++ ceru/forms.py
@@ -1,8 +1,20 @@
-from django import forms
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
+from django import forms
 
 
-class UserUpdateForm(forms.ModelForm):
+class EditProfileForm(UserChangeForm):
+    """
+    Form to allow users
+    to edit their profiles.
+    """
+    username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))  # noqa
+    email = forms.EmailField(max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control'}))  # noqa
+
     class Meta:
+        """
+        Meta class to set model used in form
+        and fields to display in form.
+        """
         model = User
-        fields = ('username', 'email',)
+        fields = ('username', 'email')
